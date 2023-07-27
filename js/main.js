5e8371dcef71b0fe195a93dc9c8a4380d442e435
Kajetan <kajetan.kowalczyk12@gmail.com>
2023-07-27 13:19:08 +0200

    completed project

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 const nav = document.querySelector(".nav");
 const navBtn = document.querySelector(".burger-btn");
 const allNavItems = document.querySelectorAll(".nav__item");
+const navBtnBars = document.querySelector(".burger-btn__bars");
+const allSections = document.querySelectorAll("section");
+const footerYear = document.querySelector(".footer__year");
+console.log(allSections);
 const handleNav = () => {
 	nav.classList.toggle("nav--active");
-
+	navBtnBars.classList.remove("black-bars-color");
 	allNavItems.forEach((item) => {
 		item.addEventListener("click", () => {
 			nav.classList.remove("nav--active");
@@ -11,7 +15,6 @@ const handleNav = () => {
 	});
 	handleNavItemAnimation();
 };
-
 const handleNavItemAnimation = () => {
 	let delayTime = 0;
 
@@ -21,4 +24,48 @@ const handleNavItemAnimation = () => {
 		delayTime++;
 	});
 };
+
+const handleObserver = () => {
+	const currentSection = window.scrollY;
+	allSections.forEach((section) => {
+		if (
+			section.classList.contains("white-section") &&
+			section.offsetTop <= currentSection + 60
+		) {
+			navBtnBars.classList.add("black-bars-color");
+		} else if (
+			!section.classList.contains("white-section") &&
+			section.offsetTop <= currentSection + 60
+		) {
+			navBtnBars.classList.remove("black-bars-color");
+		}
+	});
+};
+
+const handleObserverNav = () => {
+	const currentSection = window.scrollY;
+	allSections.forEach((section) => {
+		if (
+			section.classList.contains("white-section") &&
+			section.offsetTop <= currentSection + 60 &&
+			!nav.classList.contains("nav--active")
+		) {
+			navBtnBars.classList.add("black-bars-color");
+		} else if (
+			!section.classList.contains("white-section") &&
+			section.offsetTop <= currentSection + 60
+			// nav.classList.contains("nav--active")
+		) {
+			navBtnBars.classList.remove("black-bars-color");
+		}
+	});
+};
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+handleCurrentYear();
 navBtn.addEventListener("click", handleNav);
+navBtn.addEventListener("click", handleObserverNav);
+window.addEventListener("scroll", handleObserver);
